1bea5c945a244936f5d79dd962ca6afc95f8ae0a
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-10 13:36:30 -0500

    assignment 6 complete

--- src/index.js
+++ src/index.js
@@ -9,20 +9,10 @@ import store from './store';
 
 const main = function () {
   api.getItems()
-  .then(res => res.json())
-  .then((items) => {
-    items.forEach((item) => store.addItem(item));
-    shoppingList.render();
-  });
-
-  api.createItem('pears')
-    .then(res => res.json())
-    .then((newItem) => {
-      return api.getItems();
-    })
     .then(res => res.json())
     .then((items) => {
-      console.log(items);
+      items.forEach((item) => store.addItem(item));
+      shoppingList.render();
     });
 
   shoppingList.bindEventListeners();
