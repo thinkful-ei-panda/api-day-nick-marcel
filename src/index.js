b452e6b4cfb3a5a6563c053466635883227240a5
nickthorpe71 <nickthorpe71@gmail.com>
2020-06-10 11:39:02 -0500

    assignment 5 complete

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import shoppingList from './shopping-list';
 import api from './api';
 
 const main = function () {
-  
+  api.createItem('pears')
+    .then(res => res.json())
+    .then((newItem) => {
+      return api.getItems();
+    })
+    .then(res => res.json())
+    .then((items) => {
+      console.log(items);
+    });
 
   shoppingList.bindEventListeners();
   shoppingList.render();
