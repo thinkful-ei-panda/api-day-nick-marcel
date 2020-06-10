9f68080b0b260a1ba00db629e9cb2942d6b8843a
Marcel Woodman <woodman_marcel@yahoo.com>
2020-06-10 11:25:02 -0700

    finished assignment 5

--- src/index.js
+++ src/index.js
@@ -5,8 +5,16 @@ import './index.css';
 
 import shoppingList from './shopping-list';
 import api from './api';
+import store from './store';
 
 const main = function () {
+  api.getItems()
+  .then(res => res.json())
+  .then((items) => {
+    items.forEach((item) => store.addItem(item));
+    shoppingList.render();
+  });
+
   api.createItem('pears')
     .then(res => res.json())
     .then((newItem) => {
